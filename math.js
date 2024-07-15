eb14b9f4818ab2debd4dadc9018e55208ca91b6e
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-15 18:05:25 +0530

    index

--- math.js
+++ math.js
@@ -4,10 +4,21 @@
 
 function add(a,b) {
     return a+b;
-    }
-    function sub(a,b) {
-        return a-b;
-    }
-    module.exports=add;
-    module.exports=sub;
+}
+
+function sub(a,b) {
+    return a-b;
+}
+
+module.exports=add;
+module.exports=sub;
+
+// function add(a,b) {
+//     return a+b;
+//     }
+//     function sub(a,b) {
+//         return a-b;
+//     }
+//     module.exports=add;
+//     module.exports=sub;
     
--- result.js
+++ result.js
@@ -2,7 +2,7 @@
 
 // console.log(add(5, 3));
 
-const math=require("./math");
+math=require("./math");
 console.log(math);
 
-console.log(math.add(2,3),math.sub(3,8));
+// console.log(math.add(2,3),math.sub(3,8));
