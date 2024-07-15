b44b46f8c6409f5d8cebcaa4a10329b73db8f49b
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-15 18:06:08 +0530

    module.sub

--- result.js
+++ result.js
@@ -3,6 +3,6 @@
 // console.log(add(5, 3));
 
 math=require("./math");
-console.log(math);
+// console.log(math);
 
-// console.log(math.add(2,3),math.sub(3,8));
+console.log(math.add(2,3),math.sub(4,5));
