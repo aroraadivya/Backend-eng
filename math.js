c2d34d56d2e9d7f8dc51c02081bb63f5d07dcf1f
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-15 18:00:18 +0530

    math.js

--- math.js
+++ math.js
@@ -1,3 +1,13 @@
-function add(a,b){
+// function add(a,b){
+//     return a+b;
+// }
+
+function add(a,b) {
     return a+b;
-}
+    }
+    function sub(a,b) {
+        return a-b;
+    }
+    module.exports=add;
+    module.exports=sub;
+    
