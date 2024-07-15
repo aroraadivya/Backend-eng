5d54979d00bbf28726725d0356b3034b2ecc9afc
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-15 18:01:32 +0530

    result.js

--- result.js
+++ result.js
@@ -1,3 +1,8 @@
-const math=require("./math");
+// const math=require("./math");
 
-console.log(add(5, 3));
+// console.log(add(5, 3));
+
+const math=require("./math");
+console.log(math);
+
+console.log(math.add(2,3),math.sub(3,8));
