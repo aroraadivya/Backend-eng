936a307dea8e200e6b4cdd1842ed66c5d291b7ab
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-16 17:10:22 +0530

    indupd6

--- index.js
+++ index.js
@@ -22,11 +22,12 @@
 //     console.log(err);
 //   });
 
-const os=require("os");
-console.log(os.arch());
-const freeMemory=os.freemem();
-console.log(`${freeMemory/1024/1024/1024}`);
-const totalmem=os.totalmem();
-console.log(`${totalmem/1024/1024/1024}`);
-console.log(os.hostname());
-console.log(os.platform());
+// const os=require("os");
+// console.log(os.arch());
+// const freeMemory=os.freemem();
+// console.log(`${freeMemory/1024/1024/1024}`);
+// const totalmem=os.totalmem();
+// console.log(`${totalmem/1024/1024/1024}`);
+// console.log(os.hostname());
+// console.log(os.platform());
+
