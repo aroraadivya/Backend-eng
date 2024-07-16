3031695fef972b1f075c2491483713ed1c277596
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-16 16:41:25 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-http= require('http');
+// http= require('http');
 
-myserver= http.createServer((req, res)=>{
-    console.log(req);
-    res.end("hello from browser");
-})
-myserver.listen(8080,()=>console.log("server started"));
+// myserver= http.createServer((req, res)=>{
+//     console.log(req);
+//     res.end("hello from browser");
+// })
+// myserver.listen(8080,()=>console.log("server started"));
+
+
+const fs=require("fs");
+console.log("1");
+const result=fs.readFileSync("read.txt", "utf-8");
+console.log(result);
+
+console.log("2");
