e1e6e8fb14586bb8dab6f7830f86ced9b4314b98
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-16 17:08:19 +0530

    ind5 up

--- index.js
+++ index.js
@@ -25,7 +25,6 @@
 const os=require("os");
 console.log(os.arch());
 const freeMemory=os.freemem();
-// console.log(freeMemory);
 console.log(`${freeMemory/1024/1024/1024}`);
 const totalmem=os.totalmem();
 console.log(`${totalmem/1024/1024/1024}`);
