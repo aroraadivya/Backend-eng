b054fb463659fed11f530b20fbd1fa8a17fedec5
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-17 17:13:04 +0530

    express.js

--- express.js
+++ express.js
@@ -1,13 +1,21 @@
-const express=require("express")
-const user=require("./mock_data.json")
+const express = require("express");
+const users = require("./mock_data.json");
 
-const app=express();
-const port=7000;
+const app = express();
+const port = 7000;
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/api/user", (req, res)=>{
+// app.get("/api/users",(req,res)=>{
+//     return res.json(users)
+// })
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  if (user) {
     return res.json(user);
+  }
+  return res.status(404).json({ error: "User not found" });
 });
 
-app.listen(port, ()=>console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`server started at ${port}`));
