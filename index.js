71c4fae634c41bc3de263bf6838075483ed71c69
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-17 16:48:21 +0530

    ind upd2

--- index.js
+++ index.js
@@ -61,3 +61,16 @@
 // });
 // myserver.listen(8080, () => console.log("Server Started"));
 
+const express=require("express")
+const user=require("./mock_data.json")
+
+const app=express();
+const port=7000;
+
+app.use(express.urlencoded({extended:false}));
+
+app.get("/api/user", (req, res)=>{
+    return res.json(user);
+});
+
+app.listen(port, ()=>console.log(`Server is running on port ${port}`));
