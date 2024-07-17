58b8ca1d1d33111e7ad47e4e8e43f60de3765f74
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-17 16:59:30 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+myserver = http.createServer((req, res) => {
+    const log=`${Date.now()}:${req.url}\n`;
+  if (req.url == "/") {
+    fs.appendFile("serverLogs.txt", log, (err) => {
+      res.end("Home page");
+    });
+  } else if (req.url == "/aboutus") {
+    fs.appendFile("serverLogs.txt", log, (err) => {
+      res.end("About Us page");
+    });
+  }
+  else{
+    fs.appendFile("serverLogs.txt", log, (err) => {
+      res.end("Page not found");
+    });
+  
+  }
+});
+myserver.listen(8080, () => console.log("Server Started"));
