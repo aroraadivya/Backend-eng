34e647607fa6fb4a57139485bbd8f57ebce0a2de
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-17 16:58:22 +0530

    path.js

--- index.js
+++ index.js
@@ -11,13 +11,7 @@
 
 
 
-// const path=require("path");
-// console.log(path.dirname("index.js"));
-// console.log(path.extname("index.js"));
-// console.log(path.basename("index.js"));
-// console.log(path.parse("index.js"));
-// const par=path.parse("index.js");
-// console.log(par.name);
+
 
 const http = require("http");
 const fs = require("fs");
