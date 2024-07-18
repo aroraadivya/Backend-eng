9f5f511d7774c390e112d1c00477e3118fd6c039
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-18 16:46:38 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`)
+})
