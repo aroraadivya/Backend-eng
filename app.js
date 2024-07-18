3ae19b00470ed939c0f860ca40d0055d6830d253
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-18 17:07:34 +0530

    update app.js and index.ejs

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
+// app.set('view engine', 'ejs')
+
+// app.get('/', (req, res) => {
+//     res.render('pages/index')
+// })
+// app.listen(port, () => {
+//   console.log(`App listening at port ${port}`)
+// })
+
+
+const express = require('express')
+const app = express()
+const port = 3000
 app.set('view engine', 'ejs')
 
+const user = {
+    firstName: 'Divya',
+    lastName: 'Arora',
+}
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('pages/index', {
+        user: user
+    })
 })
 app.listen(port, () => {
   console.log(`App listening at port ${port}`)
