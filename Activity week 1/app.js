44a61581620c3306d497d00ecfcd6fc8b9f4b6b4
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-19 17:07:15 +0530

    app.js

--- Activity week 1/app.js
+++ Activity week 1/app.js
@@ -4,3 +4,15 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home',
+        message: 'Welcome to the homepage',
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App listening at port ${port}`);
+});
