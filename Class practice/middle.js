4821ad754abe071f02ab6de29cde2d1e46983e3f
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-22 17:28:47 +0530

    middleware

--- Class practice/middle.js
+++ Class practice/middle.js
@@ -1,11 +1,40 @@
+// const express = require('express')
+// const app = express()
+
+// const myLogger = function (req, res, next) {
+//   console.log('LOGGED')
+//   next()
+// }
+// app.use(myLogger)
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+// app.listen(3000)
+
+// const express = require('express')
+// const app = express()
+// const requestTime = function (req, res, next) {
+//   req.requestTime = Date.now()
+//   next()
+// }
+// app.use(requestTime)
+// app.get('/', (req, res) => {
+//   let responseText = 'Hello World!<br>'
+//   responseText += `<small>Requested at: ${req.requestTime}</small>`
+//   res.send(responseText)
+// })
+// app.listen(3000)
+
+
 const express = require('express')
-const app = express()
-const myLogger = function (req, res, next) {
-  console.log('LOGGED')
-  next()
-}
-app.use(myLogger)
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    const app = express()
+
+app.use(function(req,res,next){
+    console.log("new req rec"+Date());
+});
+
+app.get('/users', (req, res, next) => {
+    res.send('USER')
+  })
 app.listen(3000)
+
